c533f1821dd50ee18ad6e1ce359e073282053b12
Akshat Chaturvedi <akshatchaturvedi422@gmail.com>
2024-01-13 20:19:52 +0530

    Update RemoveNthNodeFromEndSLL

--- src/LinkedList/RemoveNthNodeFromLL.java
+++ src/LinkedList/RemoveNthNodeFromLL.java
@@ -1,5 +1,46 @@
 package LinkedList;
 
 public class RemoveNthNodeFromLL {
+    public class Node {
+        public int data;
+        public Node next;
+        public Node prev;
 
+        Node() {
+            this.data = 0;
+            this.next = null;
+            this.prev = null;
+        }
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+            this.prev = next;
+        }
+    }
+
+    public static Node removeKthNode(Node head, int K) {
+        Node fast = head;
+        Node slow = head;
+        for (int i = 0; i < K; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        Node delNode = slow.next;
+        slow.next = slow.next.next;
+        return head;
+    }
 }
